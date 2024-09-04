d97c440702f46125b77f4ab73bf90504e0e39864
Éric-Sébastien Lachance <eslachance@gmail.com>
2024-09-03 21:08:14 -0400

    another note?

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <h1>Test implementation : react with vite on github pages without gh-pages dependency.</h1>
+    <p>TBD</p>
     <div className="m-2 p-2 flex items-center gap-2 border-1 border-solid w-fit rounded">
       <span className="i-ic-sharp-edit-note" />
       Edit me!
