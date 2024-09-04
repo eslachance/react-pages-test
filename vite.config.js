552e9bd0621c7fed1324df57734c40edaba5bf87
Éric-Sébastien Lachance <eslachance@gmail.com>
2024-09-03 21:04:24 -0400

    vite config needs to change base URL

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import UnoCSS from 'unocss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  base: '/react-pages-test/',
   build: {
     outDir: './docs',
     emptyOutDir: true, // also necessary
