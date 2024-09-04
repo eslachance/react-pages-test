258dc4a468a8e355168f6e3721c56d83dd3a6315
Éric-Sébastien Lachance <eslachance@gmail.com>
2024-09-03 21:00:34 -0400

    files moved to docs/ folder because gh pages sucks

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import UnoCSS from 'unocss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  build: {
+    outDir: './docs',
+    emptyOutDir: true, // also necessary
+  }
 });
