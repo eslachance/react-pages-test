8b23b1e648ffc4fcfec53e7bc68da25d716d59d3
Éric-Sébastien Lachance <eslachance@gmail.com>
2024-09-03 21:07:53 -0400

    Add precommit hook and little note in app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 function App() {
   return (
+    <h1>Test implementation : react with vite on github pages without gh-pages dependency.</h1>
     <div className="m-2 p-2 flex items-center gap-2 border-1 border-solid w-fit rounded">
       <span className="i-ic-sharp-edit-note" />
       Edit me!
